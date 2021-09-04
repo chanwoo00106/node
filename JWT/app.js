4f441eb2d22904c359e94bec683f2e5fc79bbd59
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2021-09-04 15:11:33 +0900

    reset

--- JWT/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const tokenRouter = require('./routes/token');
-
-const app = express();
-
-
-app.use('/token', tokenRouter);
-
-app.listen(8000)
--- README.md
+++
@@ -1,5 +0,0 @@
-# node.js 를 공부하는 곳
-
-* [js 문법](/grammar)
-* [express](/express)
-* [JWT](/JWT)
